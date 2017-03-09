609a2d9293141d321fefdb7155b1222a4516e881
jpdyuki <joypad.y.t.h@gmail.com>
2017-03-09 17:16:49 +0800

    feat: implement to_json

--- credit_card.rb
+++ credit_card.rb
@@ -17,7 +17,10 @@ class CreditCard
   # returns json string
   def to_json
     {
-      # TODO: setup the hash with all instance vairables to serialize into json
+      number: @number,
+      expiration_date: @expiration_date,
+      owner: @owner,
+      credit_network: @credit_network
     }.to_json
   end
 
