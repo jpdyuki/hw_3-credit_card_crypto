2b03696041d0264f5ae3014de422b33ec7847b07
jpdyuki <joypad.y.t.h@gmail.com>
2017-03-17 00:56:46 +0800

    refactor: refine crypto_spec.rb

--- spec/crypto_spec.rb
+++ spec/crypto_spec.rb
@@ -3,51 +3,40 @@ require_relative '../substitution_cipher'
 require_relative '../double_trans_cipher'
 require 'minitest/autorun'
 
+all_test = [
+  {
+    encrypt_method: SubstitutionCipher::Caesar,
+    message: 'Caesar cipher'
+  },
+  {
+    encrypt_method: SubstitutionCipher::Permutation,
+    message: 'Permutation cipher'
+  },
+  {
+    encrypt_method: DoubleTranspositionCipher,
+    message: 'Double Transposition cipher'
+  }
+]
+
 describe 'Test card info encryption' do
   before do
     @cc = CreditCard.new('4916603231464963', 'Mar-30-2020', 'Soumya Ray', 'Visa')
     @key = 3
   end
 
-  describe 'Using Caesar cipher' do
-    it 'should encrypt card information' do
-      enc = SubstitutionCipher::Caesar.encrypt(@cc, @key)
-      enc.wont_equal @cc.to_s
-      enc.wont_be_nil
-    end
+  all_test.each do |test|
+    describe 'Using ' << test[:message] do
+      it 'should encrypt card information' do
+        enc = test[:encrypt_method].encrypt(@cc, @key)
+        enc.wont_equal @cc.to_s
+        enc.wont_be_nil
+      end
 
-    it 'should decrypt text' do
-      enc = SubstitutionCipher::Caesar.encrypt(@cc, @key)
-      dec = SubstitutionCipher::Caesar.decrypt(enc, @key)
-      dec.must_equal @cc.to_s
-    end
-  end
-
-  describe 'Using Permutation cipher' do
-    it 'should encrypt card information' do
-      enc = SubstitutionCipher::Permutation.encrypt(@cc, @key)
-      enc.wont_equal @cc.to_s
-      enc.wont_be_nil
-    end
-
-    it 'should decrypt text' do
-      enc = SubstitutionCipher::Permutation.encrypt(@cc, @key)
-      dec = SubstitutionCipher::Permutation.decrypt(enc, @key)
-      dec.must_equal @cc.to_s
-    end
-  end
-
-  describe 'Using Double Transposition cipher' do
-    it 'should encrypt card information' do
-      enc = DoubleTranspositionCipher::encrypt(@cc, @key)
-      enc.wont_equal @cc.to_s
-      enc.wont_be_nil
-    end
-
-    it 'should decrypt text' do
-      enc = DoubleTranspositionCipher::encrypt(@cc, @key)
-      dec = DoubleTranspositionCipher::decrypt(enc, @key)
-      dec.must_equal @cc.to_s
+      it 'should decrypt text' do
+        enc = test[:encrypt_method].encrypt(@cc, @key)
+        dec = test[:encrypt_method].decrypt(enc, @key)
+        dec.must_equal @cc.to_s
+      end
     end
   end
 end
