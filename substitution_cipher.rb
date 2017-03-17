a020f40fa1e3133217e656ec4a441560225ed88c
alan23273850 <alan23273850@gmail.com>
2017-03-17 09:17:03 +0800

    Permutation Cipher Completed!

--- substitution_cipher.rb
+++ substitution_cipher.rb
@@ -1,4 +1,7 @@
+require './credit_card.rb'
+#
 module SubstitutionCipher
+  #
   module Caesar
     # Encrypts document using key
     # Arguments:
@@ -18,15 +21,17 @@ module SubstitutionCipher
       # TODO: decrypt string using caesar cipher
     end
   end
-
+  #
   module Permutation
     # Encrypts document using key
     # Arguments:
-    #   document: String
+    #   document: CreditCard
     #   key: Fixnum (integer)
     # Returns: String
     def self.encrypt(document, key)
       # TODO: encrypt string using a permutation cipher
+      table = (0..127).to_a.shuffle(random: Random.new(key))
+      document.to_s.chars.map { |ch| table[ch.ord].chr }.join('')
     end
 
     # Decrypts String document using integer key
@@ -36,6 +41,11 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using a permutation cipher
+      table = (0..127).to_a.shuffle(random: Random.new(key))
+      table2 = Array.new(128)
+      # prestore inverse table1 into table2 to speed up
+      table.each.with_index { |x, i| table2[x] = i }
+      document.chars.map { |ch| table2[ch.ord].chr }.join('')
     end
   end
 end
