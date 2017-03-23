d01be131a858663774811eb4eace7c2ae3718d7c
alan23273850 <alan23273850@gmail.com>
2017-03-23 16:14:19 +0800

    SK_Cipher Finished~~

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+# Add all the gems your code and tests use here!
 gem 'json'
 gem 'minitest'
-# Add all the gems your code and tests use here!
+gem 'rbnacl-libsodium'
--- sk_cipher.rb
+++ sk_cipher.rb
@@ -1,17 +1,28 @@
 require 'rbnacl/libsodium'
+require 'base64'
 
+# Usage:
+# Step1)  key = ModernSymmetricCipher.generate_new_key
+# Step2)  cipher = ModernSymmetricCipher.encrypt(doc,key)
+# Result) assert(doc == ModernSymmetricCipher.decrypt(cipher,key))
 module ModernSymmetricCipher
   def self.generate_new_key
-    # TODO: Return a new key as a Base64 string
+    # DO: Return a new key as a Base64 string
+    key = RbNaCl::Random.random_bytes(RbNaCl::SecretBox.key_bytes)
+    Base64.encode64(key)
   end
 
   def self.encrypt(document, key)
-    # TODO: Return an encrypted string
-    #       Use base64 for ciphertext so that it is sendable as text
+    # DO: Return an encrypted string
+    #     Use base64 for ciphertext so that it is sendable as text
+    simple_box = RbNaCl::SimpleBox.from_secret_key(Base64.decode64(key))
+    Base64.encode64(simple_box.encrypt(document))
   end
 
   def self.decrypt(aes_crypt, key)
-    # TODO: Decrypt from encrypted message above
-    #       Expect Base64 encrypted message and Base64 key
+    # DO: Decrypt from encrypted message above
+    #     Expect Base64 encrypted message and Base64 key
+    simple_box = RbNaCl::SimpleBox.from_secret_key(Base64.decode64(key))
+    simple_box.decrypt(Base64.decode64(aes_crypt))
   end
 end
